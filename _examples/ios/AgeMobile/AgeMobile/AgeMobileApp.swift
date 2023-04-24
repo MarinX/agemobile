7bbbb9a02542ae45993d2c33287dd8b304a407e3
Markus Zoppelt <markus@zoppelt.net>
2023-04-24 14:21:31 +0200

    feat: add ios example project

---
+++ _examples/ios/AgeMobile/AgeMobile/AgeMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeMobileApp.swift
+//  AgeMobile
+//
+//  Created by Markus Zoppelt on 24.04.23.
+//
+
+import SwiftUI
+
+@main
+struct AgeMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
